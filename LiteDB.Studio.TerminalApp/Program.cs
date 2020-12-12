762005723f147aff265c27c5031b02be79a174b5
hmadrigal <hfmad@hotmail.com>
2020-12-12 01:07:24 -0600

    Adding terminal app based on ReactUI+Terminal.Gui

---
+++ LiteDB.Studio.TerminalApp/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Reactive.Concurrency;
+using LiteDB.Studio.TerminalApp.Views;
+using LiteDB.Studio.ViewModels;
+using ReactiveUI;
+using Terminal.Gui;
+
+namespace LiteDB.Studio.TerminalApp
+{
+    class Program
+    {
+        public static Action running = MainApp;
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+            while (running != null)
+            {
+                running.Invoke();
+            }
+            Application.Shutdown();
+        }
+
+
+        static void MainApp()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+            if (System.Diagnostics.Debugger.IsAttached)
+                System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+
+            Application.Init();
+
+            RxApp.MainThreadScheduler = TerminalScheduler.Default;
+            RxApp.TaskpoolScheduler = TaskPoolScheduler.Default;
+
+            Application.Run(new LoginView(new LoginViewModel()));
+        }
+    }
+}
