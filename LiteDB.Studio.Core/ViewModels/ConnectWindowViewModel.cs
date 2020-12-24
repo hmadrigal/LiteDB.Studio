f38c1b36c280d6a2f98a8f0274968f961be0159c
hmadrigal <hfmad@hotmail.com>
2020-12-23 23:45:30 -0600

    Using MainWIndowViewModel from Core project

---
+++ LiteDB.Studio.Core/ViewModels/ConnectWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using NStack;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace LiteDB.Studio.ViewModels
+{
+    [DataContract]
+    public class ConnectWindowViewModel : ReactiveObject
+    {
+
+    }
+}
---
+++ LiteDB.Studio.Core/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using NStack;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace LiteDB.Studio.ViewModels
+{
+    [DataContract]
+    public class MainWindowViewModel : ViewModelBase
+    {
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> ShowConnectWindowCommand { get; }
+
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> RefreshCommand { get; }
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> RunCommand { get; }
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> BeginCommand { get; }
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> CommitCommand { get; }
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> RollbackCommand { get; }
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> CheckpointCommand { get; }
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> DebugCommand { get; }
+
+        public string Greeting => "Hello World!";
+        public MainWindowViewModel()
+        {
+
+        }
+
+    }
+}
